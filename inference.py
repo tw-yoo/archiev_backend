a243e20c06193d37e3a58cd24b6e5ae82d6b65b3
Taewon Yoo <twyoo@autocrypt.io>
2023-05-12 18:27:25 +0900

    commit before submit

--- inference.py
+++ inference.py
@@ -3,6 +3,7 @@ import io
 from urllib import request
 import numpy as np
 from PIL import Image
+import pandas as pd
 
 from emission_result import LabelAndProb
 
@@ -42,9 +43,16 @@ def get_resized_np_array(uid, image, input_size):
     tracker = EmissionsTracker(project_name="step3", output_file=f"{uid}.csv")
     tracker.start()
 
-    image = image.resize((480, 480))
+    ratio = 15
+    size1 = image.size[0]
+    size2 = image.size[1]
+
+    image = image.resize((size1 * ratio, size2 * ratio))
+
+    image = image.resize((input_size, input_size))
     x = np.array(image)
     x = x / 255.0
+    x = x[:, :, :3]
 
     x = np.expand_dims(x, axis=0)
 
@@ -85,7 +93,19 @@ def convert_inference_result(probs):
             LabelAndProb(k, float(result[k])).__dict__
         )
 
-    return label_and_prob_list
+    label_and_prob_df = pd.DataFrame().from_records(label_and_prob_list).sort_values(['prob'], ascending=False)[:10]
+    label_and_prob_df_list = []
+
+    for i, r in label_and_prob_df.iterrows():
+        label_and_prob = LabelAndProb(
+            label = r['label'],
+            prob = r['prob']
+        )
+        label_and_prob_df_list.append(label_and_prob.__dict__)
+
+    # return label_and_prob_list
+    return label_and_prob_df_list
+
 
 
 def get_model_url_and_size(model_name, model_df):
